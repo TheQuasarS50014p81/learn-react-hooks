fafa380e7d5405d621ca58248117026c12d88fb4
The Quasar <20722202+TheQuasarS50014p81@users.noreply.github.com>
2021-08-29 21:21:31 +0900

    first react hooks

--- src/App.js
+++ src/App.js
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const App = () => {
+  const [count, setCount] = useState(0)
+  
+  const increment = () => setCount(count + 1)
+  const decrement = () => setCount(count - 1)
+
+  const increment2 = () => setCount(previousCount => previousCount + 1)
+  const decrement2 = () => setCount(previousCount => previousCount - 1)
+  const reset = () => setCount(initialize => initialize = 0)
+  const multiple = () => setCount(multiplication => multiplication * 2)
+  const multipleOnThree = () => setCount(value => value % 3 === 0 ? value / 3 : value)
+
   return (
-    <div>
-      This is a template for Reacr App.
-    </div>
+    <React.Fragment>
+      <div>count: {count}</div>
+      <div>
+        <button onClick={increment}>+1</button>
+        <button onClick={decrement}>-1</button>
+      </div>
+      <div>
+        <button onClick={increment2}>+1</button>
+        <button onClick={decrement2}>-1</button>
+      </div>
+      <div>
+        <button onClick={reset}>Reset</button>
+        <button onClick={multiple}>Multiple</button>
+        <button onClick={multipleOnThree}>3の倍数</button>
+      </div>
+    </React.Fragment>
   );
 }
 
